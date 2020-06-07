d4358d2ef26c543d80df41d7897c4ce697c63522
enkr1 <enkr99@gmail.com>
2020-06-07 17:35:36 +0800

    Update main.js

--- main.js
+++ main.js
@@ -12,7 +12,7 @@ $(document).ready(function () {
         const preload = document.querySelector('.preload');
         preload.classList.add('preload-finish');
         // only when finish loading, add y scroll 
-        var root = this.getElementsByTagName('html')[0]; // '0' to assign the first (and only `HTML` tag)
+        var root = document.getElementsByTagName('html')[0]; // '0' to assign the first (and only `HTML` tag)
         root.classList.add('enablescroll');
         // root.classList.remove('unablescroll');
     });
