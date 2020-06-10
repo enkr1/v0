dfb3c702ba7c382ce7f5ef28c201e62f88818883
enkr1 <enkr99@gmail.com>
2020-06-10 17:18:47 +0800

    -

--- main.js
+++ main.js
@@ -26,6 +26,48 @@ $(document).ready(function () {
         $('.top-nav').removeClass('open');
     });
 
+
+    // dev ed https://www.youtube.com/watch?v=RLc8NB2JyxE
+    // const sections = document.querySelectorAll('section');
+    // const userposition = document.querySelector('.userposition');
+
+    // const options = {
+    //     threshold: 0.5
+    // };
+
+    // let observer = new IntersectionObserver(navCheck, options);
+
+    // function navCheck(entries) {
+    //     entries.forEach(entry => {
+    //         const className = entry.target.className; 
+    //         console.log(className)
+    //         const activeAnchor = document.querySelector(`[data-page=${className}]`);
+    //         const targetIndex = entry.target.getAttribute('data-index');
+    //         const coords = activeAnchor.getBoundingClientRect();
+    //         const directions = {
+    //             height: coords.height/4,
+    //             width: coords.width,
+    //             top: coords.top,
+    //             left: coords.left
+    //         };
+    //         if(entry.isIntersecting) {
+    //             userposition.style.setProperty('left',`${directions.left}px`);
+    //             userposition.style.setProperty('top',`${directions.top}px`);
+    //             userposition.style.setProperty('width',`${directions.width}px`);
+    //             userposition.style.setProperty('height',`${directions.height}px`);
+    //         }
+            
+            
+    //     });
+    // }
+
+    // sections.forEach(section => {
+    //     observer.observe(section);
+    // });
+
+
+
+
     // Handle ESC key (key code 27)
     document.addEventListener('keyup', function (e) {
         $('.menu-toggler').removeClass('open');
