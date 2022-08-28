ecf0fc29f99cfdf43c45a192c3abe8c1c5a92d73
Jing Hui P <enkr99@gmail.com>
2022-08-29 00:57:25 +0800

    :sparkles: [portfolio] Integrated SwiperJs

--- js/components/portfolio-items.js
+++ js/components/portfolio-items.js
@@ -20,10 +20,10 @@ class PortfolioItem {
             `</picture>` +
 
             `<div class="portfolio-content">` +
-            `<h1 class="portfolio-title">${this.category}</h1>` +
+            `<h1 class="portfolio-project_name">${this.projectName}</h1>` +
 
             `<div class="portfolio-description">` +
-            `<span class="portfolio-date">${this.projectName}</span>` +
+            `<span class="portfolio-category">${this.category}</span>` +
             `<p>${this.desc}</p>` +
             `<a target="_blank" href="${this.ridirectionPath}" class="cta">${this.buttonName}</a>` +
             `</div>` +
@@ -40,7 +40,6 @@ class PortfolioItem {
 
 var listOfItems = [];
 
-
 listOfItems.push(
     new PortfolioItem(
         "Web Design & Development",
@@ -52,7 +51,7 @@ listOfItems.push(
         `,
         "images/portfolio/555_website.png",
         "https://enkr1.github.io/555beatbox/",
-        "click me"
+        "view project"
     ),
     new PortfolioItem(
         "Web Design",
@@ -64,7 +63,7 @@ listOfItems.push(
         `,
         // The design of this website will be a solution to help users find relevant information using
         // principles like visual hierarchy, colours, typography and images.
-        // The new design of the website provides some unique features to attract users to find it useful. 
+        // The new design of the website provides some unique features to attract users to find it useful.
         "images/portfolio/portitem1.jpg",
         "https://xd.adobe.com/view/70da2996-2e62-4834-7d54-20ed619382b6-06eb/",
         "view project"
@@ -76,9 +75,9 @@ listOfItems.push(
         I designed many projects and I customised them as well. This is
         one of my favourite products. I do product design and sketching.
         `,
-        "images/portfolio/portitem3.png",
+        "images/portfolio/portitem3.jpg",
         "https://www.instagram.com/p/B0YcuLVnnlu/",
-        "click me"
+        "view on Instagram"
     ),
 
 );
@@ -102,15 +101,16 @@ class PortfolioItemsComponent extends HTMLElement {
         this.innerHTML = `
         <div class="swiper portfolio-items-swiper">
             <div class="swiper-wrapper">
-            ${this.innerHTML}
+                ${this.innerHTML}
             </div>
-            <div class="swiper-pagination"></div>
         </div>`;
 
 
+        // <div class="swiper-pagination"></div>
+        // <div class="swiper portfolio-items-swiper">
         // NOTE: Put at the end of the .swiper if you wanna use these
         // <div class="swiper-pagination"></div>
-        // <div class="swiper-button-prev"></div> 
+        // <div class="swiper-button-prev"></div>
         // <div class="swiper-button-next"></div>
 
         // <div class="swiper-scrollbar"></div>
@@ -123,46 +123,47 @@ customElements.define('portfolio-items-component', PortfolioItemsComponent);
 
 // Official site: https://swiperjs.com/get-started
 // gh: https://github.com/nolimits4web/swiper/
-// const porfolioItemsSwiper = new Swiper('.portfolio-items-swiper', {
-//     grabCursor: true,
-//     spaceBetween: 0,
-//     centeredSlides: true,
-//     direction: "horizontal",
-//     slidesPerView: 4,
-//     // slidesPerView: (screen.width > 992) ? "auto" : 1, // TODO: Check if this is what causes the problem
-//     loop: true,
-//     lazy: true,
-//     mousewheel: false, // true is good for vertical 
-//     freeMode: false, // scroll without section parting
+const porfolioItemsSwiper = new Swiper('.portfolio-items-swiper', {
+    grabCursor: true,
+    spaceBetween: 10,
+    centeredSlides: true,
+    direction: "horizontal",
+    slidesPerView: 1.1,
+    // effect: "coverflow",
+    // slidesPerView: (screen.width > 992) ? "auto" : 1.1, // TODO: Check if this is what causes the problem
+    loop: true,
+    lazy: true,
+    mousewheel: false, // true is good for vertical
+    freeMode: false, // scroll without section parting
 
-//     keyboard: {
-//         enabled: true,
-//     },
+    keyboard: {
+        enabled: true,
+    },
 
-//     hashNavigation: {
-//         watchState: true,
-//     },
-//     history: false,
+    hashNavigation: {
+        watchState: true,
+    },
+    history: false,
 
-//     // If we need pagination
-//     pagination: {
-//         el: '.swiper-pagination',
-//         clickable: true,
-//         dynamicBullets: true,
-//         // renderBullet: function (index, className) {
-//         //   return '<span class="' + className + '">' + (index + 1) + "</span>";
-//         // },
-//     },
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: false,
+        dynamicBullets: false,
+        // renderBullet: function (index, className) {
+        //   return '<span class="' + className + '">' + (index + 1) + "</span>";
+        // },
+    },
 
-//     // Navigation arrows
-//     navigation: {
-//         nextEl: '.swiper-button-next',
-//         prevEl: '.swiper-button-prev',
-//     },
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
 
-//     // And if we need scrollbar
-//     scrollbar: {
-//         el: '.swiper-scrollbar',
-//         hide: false,
-//     },
-// });
+    // And if we need scrollbar
+    scrollbar: {
+        el: '.swiper-scrollbar',
+        hide: true,
+    },
+});
